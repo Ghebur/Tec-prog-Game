6bc7e73ecb4ae45a9009d5a50a6f4be75488158e
Ghebur <glauciusfilho90@gmail.com>
2026-06-10 20:48:57 -0300

    Fix: removendo de vez a pasta build do rastreamento

--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj: \
- C:\Users\nicol\.vscode\codigos_proprios\Tec-prog-Game\build\_deps\sfml-src\src\SFML\Graphics\BlendMode.cpp \
- C:/Users/nicol/.vscode/codigos_proprios/Tec-prog-Game/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
- C:/Users/nicol/.vscode/codigos_proprios/Tec-prog-Game/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- C:/Users/nicol/.vscode/codigos_proprios/Tec-prog-Game/build/_deps/sfml-src/include/SFML/Config.hpp
--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.obj: \
- C:\Users\nicol\.vscode\codigos_proprios\Tec-prog-Game\build\_deps\sfml-src\src\SFML\Graphics\StencilMode.cpp \
- C:/Users/nicol/.vscode/codigos_proprios/Tec-prog-Game/build/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
- C:/Users/nicol/.vscode/codigos_proprios/Tec-prog-Game/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- C:/Users/nicol/.vscode/codigos_proprios/Tec-prog-Game/build/_deps/sfml-src/include/SFML/Config.hpp
